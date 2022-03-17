8114bbce844e3d2ce99876411b6fdc5ce6f9b827
Matthew Patrick <mattimpat@gmail.com>
2022-03-16 21:52:20 -0400

    Added code to extract comorbidities for our rare diseases

--- blairEnrichment.sh
+++ blairEnrichment.sh
@@ -1,3 +1,4 @@
+#Mendelian diseases from Blair et al.
 mendDis=$(wc -l ../data/blair/* | awk '/m/&&$1!=0{print $2}')
 commDis=$(wc -l ../data/blair/* | awk '/c/&&$1!=0{print $2}')
 
@@ -16,3 +17,24 @@ for x in $mendDis; do
                 awk 'NR==FNR{pat[$1]=1;next} ($1 in pat)' $x $y | wc -l | tr '\n' '\t'
         done | sed 's/\t$/\n/'
 done; } > ../data/blairMendComm.dat
+
+
+#Rare diseases from our study
+mendDis=$(ls ../data/pats/*)
+commDis=$(wc -l ../data/blair/* | awk '/c/&&$1!=0{print $2}')
+
+{ echo "$mendDis" | xargs -I{} basename {} | tr '\n' '\t' | sed 's/\t$/\n/'
+for x in $mendDis; do
+        basename $x | tr '\n' '\t'
+        for y in $mendDis; do
+                awk 'NR==FNR{pat[$1]=1;next} ($1 in pat)' $x $y | wc -l | tr '\n' '\t'
+        done | sed 's/\t$/\n/'
+done; } > ../data/blairMendMend_our.dat
+
+{ echo "$commDis" | xargs -I{} basename {} | tr '\n' '\t' | sed 's/\t$/\n/'
+for x in $mendDis; do
+        basename $x | tr '\n' '\t'
+        for y in $commDis; do
+                awk 'NR==FNR{pat[$1]=1;next} ($1 in pat)' $x $y | wc -l | tr '\n' '\t'
+        done | sed 's/\t$/\n/'
+done; } > ../data/blairMendComm_our.dat
